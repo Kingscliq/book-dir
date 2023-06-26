52a67f2ba707547bc1a3d1363ac45598b6203e8f
michael <michaelblossom8654@gmail.com>
2023-06-27 05:24:48 +0530

    setup build

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,34 @@
-import express, { Request, Response } from 'express';
+// import express, { Request, Response } from 'express';
+
+// const app = express();
+// const PORT = 4040;
+
+// app.get('/api', (req: Request, res: Response) => {
+//   return res.send('Hello Ekene');
+// });
+
+// app.listen(PORT, () => console.log(`Server Started! as ${PORT} `));
+
+import express from 'express'
+import morgan from 'morgan'
+
+
 
 const app = express();
-const PORT = 4040;
 
-app.get('/api', (req: Request, res: Response) => {
-  return res.send('Hello Ekene');
-});
 
-app.listen(PORT, () => console.log(`Server Started! as ${PORT} `));
+// Global MIDDLEWARES'
+// develepment login
+//console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+
+// body parser
+app.use(express.json({ limit: "10kb" })); 
+
+app.use(express.static(`${__dirname}/public`)); //for serving static files
+
+
+export default app
---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import mongoose, { ConnectOptions } from "mongoose";
+import dotenv from 'dotenv'
+import app from './app'
+
+dotenv.config({ path: "./config.env" }); 
+
+const DB = process.env.DATABASE!.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD!
+  );
+  mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions)
+    .then((con) => {
+      console.log("Db connection successful");
+    });
+
+const port = 4040;
+const server = app.listen(port, () => {
+  console.log(`App running in port ${port}...`);
+});
+
+
