bf3c99fd6a6ae6c34627308bf31df09e1100bb16
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-06-28 18:09:31 +0100

    update ts config cleanup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,16 @@
-import express from 'express'
-import morgan from 'morgan'
+import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 // Global MIDDLEWARES'
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // body parser
-app.use(express.json({ limit: "10kb" })); 
+app.use(express.json({ limit: '10kb' }));
 
 app.use(express.static(`${__dirname}/public`)); //for serving static files
 
-export default app
+export default app;
