4f5c214473ed2a94e200d5d37ad991ff8aae8151
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-06-29 07:44:30 +0100

    update: interfaces

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import { IUser } from '../interfaces/user.interface';
 
-const User = new mongoose.Schema({
+const User = new mongoose.Schema<IUser>({
   firstName: { type: String },
   lastName: { type: String },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
+  avatar: { type: String },
 });
 
 export default User;
