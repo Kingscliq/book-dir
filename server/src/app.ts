142bbfeab1c97cc6cf690aaadb6845f131057235
michael <michaelblossom8654@gmail.com>
2023-07-01 17:17:53 +0530

    defined middleware function for handlling unhandlled routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+
 import morgan from 'morgan';
 
 import bookRouter from "./routes/book.route"
@@ -16,6 +18,13 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`)); //for serving static files
 
 app.use("/api/v1/books", bookRouter);
+
+app.all("*", (req:Request, res:Response, next:NextFunction) => {
+  res.status(404).json({
+    status: 'fail',
+    message:`Can't find ${req.originalUrl} on this saver`
+  });
+})
 
 
 export default app
--- server/src/controllers/book.controller.ts
+++ server/src/controllers/book.controller.ts
@@ -2,21 +2,58 @@ import { Request, Response, NextFunction } from 'express';
 import Book from './../models/book.model';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
+import { limits } from 'argon2';
 
 // get one book
 const getAllBook = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     // filterinh the query
     const queryObj = {...req.query}
-    const excludedFields = ['page','limit','sort','fields']
+    const excludedFields:any[] = ['page','limit','sort','fields']
     excludedFields.forEach(exfields => delete queryObj[exfields])
   // console.log(req.query,queryObj)
 
   // advanced filtering
   let queryStr = JSON.stringify(queryObj)
   queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+
     // const books = await Book.find(queryObj);
-    const query =  Book.find(JSON.parse(queryStr));
+    let query =  Book.find(JSON.parse(queryStr));
+
+      // SORTING
+    // if(req.query.sort){
+    //   const sortBy = req.query.sort.split(',').join(" ")
+    //   query = query.sort(sortBy)
+    // }else{
+    //   query.sort('-createdAt')
+    // }
+
+    // FIELD LIMITING
+
+    // if(req.query.fields){
+    //   const fields = req.query.fields.split(',').join(" ")
+    //   query = query.select(fields)
+    // }else{
+    //   query.select('-__v')
+    // }
+
+      // PAGINATION
+      // const page:number = req.query.page * 1 || 1
+      // const limit:number = req.query.limit * 1 || 100
+      // const skip:number = (page - 1) * limit
+      // query = query.skip(skip).limit(limit)
+      
+      // if(req.query.page){
+      //   // countDocuments()
+      //   const numBooks = await Book.estimatedDocumentCount()
+      //   if(skip >= numBooks){
+      //     return next(new AppError(
+      //       "The page you request does not exist",
+      //       404,
+      //     ),)
+      //   }
+      // }
+
     // executing the query
     const books = await query
 
