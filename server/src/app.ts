956701ac8884d117a356ff1c61b781a3be1b91e7
michael <michaelblossom8654@gmail.com>
2023-07-01 18:05:31 +0530

    implemented global handlling middlewara

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { response } from 'express';
+// import type { ErrorRequestHandler } from 'express';
 import { Request, Response, NextFunction } from 'express';
 
 import morgan from 'morgan';
@@ -19,12 +20,30 @@ app.use(express.static(`${__dirname}/public`)); //for serving static files
 
 app.use("/api/v1/books", bookRouter);
 
+// Handling undefined routes
 app.all("*", (req:Request, res:Response, next:NextFunction) => {
-  res.status(404).json({
-    status: 'fail',
-    message:`Can't find ${req.originalUrl} on this saver`
-  });
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message:`Can't find ${req.originalUrl} on this saver!`
+  // });
+
+  const err:any = new Error(`Can't find ${req.originalUrl} on this saver!`)
+  err.status = 'fail'
+  err.statusCode = 404
+  next(err)
+
 })
+
+// Global Error Handling
+app.use((err:any,req:Request,res:Response,next:NextFunction) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+}) 
 
 
 export default app
